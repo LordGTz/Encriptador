def407c40960c25dbaf77b12f27fb751ff4cc6df
Torrez <garciatorrezroberto@gmail.com>
2022-09-13 11:57:28 -0300

    "There aren't any important change"

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let encriptar = document.getElementById("encriptar");
+let desencriptar = document.getElementById("desencriptar");
+let codigo = ["enter","imes","ai","ober","ufat"];
+let entrada = document.getElementById("entrada");
+
+if(document.getElementById("entrada").value.lenght < 10){
+    console.log("hello")
+}
