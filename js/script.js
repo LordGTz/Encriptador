964fc083b51781e8c7e6dedf2678d57841323d5c
Torrez <garciatorrezroberto@gmail.com>
2022-09-16 09:38:48 -0300

    "Tratando de realizar el Js"

--- js/script.js
+++ js/script.js
@@ -2,7 +2,3 @@ let encriptar = document.getElementById("encriptar");
 let desencriptar = document.getElementById("desencriptar");
 let codigo = ["enter","imes","ai","ober","ufat"];
 let entrada = document.getElementById("entrada");
-
-if(document.getElementById("entrada").value.lenght < 10){
-    console.log("hello")
-}
