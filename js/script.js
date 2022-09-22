3287b3d27dee3b5f4ea997abe474940665962f4a
Torrez <garciatorrezroberto@gmail.com>
2022-09-22 15:48:11 -0300

    guardado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 let encriptar = document.getElementById("encriptar");
 let desencriptar = document.getElementById("desencriptar");
-let codigo = ["enter","imes","ai","ober","ufat"];
 let entrada = document.getElementById("entrada");
+let resultado = document.getElementById("resultado")
+
+let codigo = ["enter","imes","ai","ober","ufat"];
+let mensaje;
+
+encriptar.addEventListener("click", (event)=>{
+    event.preventDefault();
+    mensaje = entrada.value;
+})
+
+/*
+desencriptar.addEventListener("click", (event)=>{
+    event.preventDefault(); 
+    resultado.value = mensaje;
+})
+*/
