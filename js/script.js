25611570b2abd74583b882c03347ddb42be01da6
Torrez <garciatorrezroberto@gmail.com>
2022-09-23 14:46:10 -0300

    Document change to version 1.01

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 let encriptar = document.getElementById("encriptar");
 let desencriptar = document.getElementById("desencriptar");
+let copiar = document.getElementById("copiar")
 let entrada = document.getElementById("entrada");
 let resultado = document.getElementById("resultado")
 
 let codigo = ["enter","imes","ai","ober","ufat"];
 let mensaje;
-
+let revision = [];
 encriptar.addEventListener("click", (event)=>{
     event.preventDefault();
-    mensaje = entrada.value;
+    mensaje = entrada.value
+    revision = [];
+    for(letras of mensaje){
+    switch(letras){
+        case "a": revision.push(codigo[0]);break;
+        case "e": revision.push(codigo[1]);break;
+        case "i": revision.push(codigo[2]);break;
+        case "o": revision.push(codigo[3]);break;
+        case "u": revision.push(codigo[4]);break;
+        default : revision.push(letras)
+    }
+    resultado.value = revision.join("")
+}})
+entrada.addEventListener("keyup",()=>{
+    if(entrada.value.length > 0){
+        document.getElementById("con-resultado").style.display = "block"
+        document.getElementById("sin-resultado").style.display = "none"
+    }else{
+        resultado.value = ""
+        document.getElementById("con-resultado").style.display = "none"
+        document.getElementById("sin-resultado").style.display = "block"
+    }
 })
-
-/*
-desencriptar.addEventListener("click", (event)=>{
-    event.preventDefault(); 
-    resultado.value = mensaje;
-})
-*/
